df061850b77faa78e530b00f32b9b1134a822599
Qu1lby <kilian.cuny@hotmail.fr>
2014-02-08 21:46:19 +0100

    Indentation

--- ennemis.js
+++ ennemis.js
@@ -9,8 +9,7 @@
 		step: function(dt) {        
         	var dirX = this.p.vx/Math.abs(this.p.vx);
             var ground = Q.stage().locate(this.p.x, this.p.y + this.p.h/2 + 1, Q.SPRITE_DEFAULT);
-            var nextTile = Q.stage().locate(this.p.x + dirX * this.p.w/2 + dirX, this.p.y + this.p.h/2 + 1, Q.SPRITE_DEFAULT);
-                
+            var nextTile = Q.stage().locate(this.p.x + dirX * this.p.w/2 + dirX, this.p.y + this.p.h/2 + 1, Q.SPRITE_DEFAULT);   
 				
             if(!nextTile && ground) {
             	if(this.p.vx > 0) {
@@ -45,6 +44,6 @@
             } 
             else if(-this.p.y + this.p.initialY >= this.p.rangeY && this.p.vy < 0) {
                  	this.p.vy = -this.p.vy;
-                 } 
-            }
-        });
+        	} 
+		}
+	});
