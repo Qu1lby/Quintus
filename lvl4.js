31c499bbeade529c133badc8ea9da918c491390b
Dysgenics <boss5.lo@hotmail.fr>
2014-02-13 14:32:50 +0100

    lvl 4

--- lvl4.js
+++ lvl4.js
@@ -1,51 +1,99 @@
-//setup level 1
-Q.scene("level4",function(stage) {
+        //setup level 2, @TODO make it so that it could be multiple levels?
+        Q.scene("level4",function(stage) {
 				
-            var background = new Q.TileLayer({ 
-				dataAsset: "Lvl4.tmx", 
-				layerIndex: 0, 
-				sheet: "tiles", 
-				tileW: 70, tileH: 70, 
-				type: Q.SPRITE_NONE });
-				
+            var background = new Q.TileLayer({ dataAsset: "Lvl4.tmx", layerIndex: 0, sheet: "tiles", tileW: 70, tileH: 70, type: Q.SPRITE_NONE });
             stage.insert(background);
            
             stage.collisionLayer(new Q.TileLayer({ dataAsset: "Lvl4.tmx", layerIndex:1,  sheet: "tiles", tileW: 70, tileH: 70, type: Q.SPRITE_DEFAULT }));
             
             var player = stage.insert(new Q.Orange());
             
+            //level assets. format must be as shown: [[ClassName, params], .. ] 
             var levelAssets = [
 			
-				["Sol_5", {x: 105 , y : 245}],
-				["Sol_5", {x: 175 , y : 245}],
-				["Sol_5", {x: 245 , y : 245}],
-				["Sol_pierre1_D", {x: 315 , y : 245}],
-				["Sol_pierre3_D", {x: 385 , y : 245}],
-				["Sol_pierre2_D", {x: 455 , y : 245}],
-				["Sol_pierre3_D", {x: 525 , y : 245}],
-				["Sol_pierre2_D", {x: 595 , y : 245}],
-				["Sol_pierre1_D", {x: 665 , y : 245}],
-				["Sol_5", {x: 735 , y : 245}],
-				["Sol_5", {x: 805 , y : 245}],
-				
-				// a supr
-				["Sol_2", {x: 945 , y : 245}],
-				
-				["Sol_2", {x: 1015 , y : 245}],
-				["Sol_2", {x: 1085 , y : 245}],
-				["Sol_2", {x: 1155 , y : 245}],
-
-				["Sol_pierre1_D", {x: 1295 , y : 175}],
-				["Sol_pierre1_D", {x: 1505 , y : 175}],
-				["Sol_5", {x: 1645 , y : 315}],
-
-				["Sol_pierre3_D", {x: 1855 , y : 665}],			
-								
-				["GroundEnemy", {x: 1640 , y : 245, asset: "slime2.png"}],
-				
-            ];
+			//Petit ilot a gauche (rdc)
+			["Sol_pierre1_D", {x: 315 , y : 1575}],
+			["Sol_pierre1_D", {x: 385 , y : 1575}],
+			["Sol_pierre1_D", {x: 385 , y : 1505}],
+			["Sol_pierre1_D", {x: 455 , y : 1435}],
+			["Sol_pierre1_D", {x: 525 , y : 1435}],
+			["Sol_pierre1_D", {x: 525 , y : 1365}],
+			["Sol_pierre1_D", {x: 595 , y : 1435}],
+			["Sol_pierre1_D", {x: 595 , y : 1505}],
+			["Sol_pierre1_D", {x: 595 , y : 1575}],
+			//["BOUTON TA RACE", {x: 525 , y : 1575}],
+			
+			//["UN PONT ICI", {x: 735 , y : 1575}],
+			//["UN PONT ICI", {x: 805 , y : 1575}],
+			//["UN PONT ICI", {x: 875 , y : 1575}],
+			//["UN PONT ICI", {x: 945 , y : 1575}],
+			
+			// Apres le pont a gauche (rdc)
+			["Sol_pierre1_D", {x: 1085 , y : 1645}],
+			["Sol_pierre2_D", {x: 1155 , y : 1645}],
+			["Sol_pierre2_D", {x: 1225 , y : 1645}],
+			["Sol_pierre1_D", {x: 1295 , y : 1645}],
+			
+			// Premier etage a gauche
+			
+			["Sol_pierre2_D", {x: 1155 , y : 1155}],
+			["Sol_pierre1_D", {x: 1085 , y : 1155}],
+			["Sol_pierre1_D", {x: 1015 , y : 1155}],
+			//["WHATTTTTTT", {x: 945 , y : 1155}],
+			["Sol_pierre1_D", {x: 875 , y : 1155}],
+			["Sol_pierre2_D", {x: 805 , y : 1155}],
+			["Sol_pierre1_D", {x: 805 , y : 1085}],
+			["Sol_pierre1_D", {x: 735 , y : 1085}],
+			["Sol_pierre2_D", {x: 665 , y : 1085}],
+			["Sol_pierre1_D", {x: 595 , y : 1085}],
+			["Sol_pierre1_D", {x: 595 , y : 1085}],
+			["Sol_pierre2_D", {x: 455 , y : 1085}],
+			["Sol_pierre1_D", {x: 385 , y : 1085}],
+			["Sol_pierre2_D", {x: 315 , y : 1085}],
+			
+			//Deuxieme etage 
+			["Sol_pierre1_D", {x: 805 , y : 595}],
+			["Sol_pierre1_D", {x: 805 , y : 525}],
+			["Sol_pierre1_D", {x: 805 , y : 455}],
+			["Sol_pierre1_D", {x: 805 , y : 385}],
+			["Sol_pierre1_D", {x: 735 , y : 525}],
+			["Sol_pierre1_D", {x: 735 , y : 455}],
+			["Sol_pierre1_D", {x: 665 , y : 595}],
+			["Sol_pierre1_D", {x: 665 , y : 525}],
+			["Sol_pierre1_D", {x: 595 , y : 595}],
+			["Sol_pierre1_D", {x: 525 , y : 595}],
+			["Sol_pierre1_D", {x: 455 , y : 595}],
+			["Sol_pierre2_D", {x: 385 , y : 595}],
+			
+			["Sol_pierre1_D", {x: 1015 , y : 595}],
+			["Sol_pierre1_D", {x: 1085 , y : 595}],
+			["Sol_pierre2_D", {x: 1155 , y : 595}],
+			["Sol_pierre1_D", {x: 1225 , y : 595}],
+			//["Sol_pierre4_D", {x: 1295 , y : 595}],
+			//........ Mass sol pierre 4d
+			//["Sol_pierre4_D", {x: 2625 , y : 595}],
+			
+			//sous sol deuxieme etage
+			["Sol_pierre1_D", {x: 455 , y : 595}],
+			
+			["Sol_pierre1_D", {x: 1715 , y : 875}],
+			//........  Mass sol pierre 1d
+			["Sol_pierre1_D", {x: 2695 , y : 875}],
+			
+			
+			//Petit ilot de droite
+			["Sol_pierre2_D", {x: 2905 , y : 945}],
+			["Sol_pierre1_D", {x: 3045 , y : 945}],
+			["Sol_pierre1_D", {x: 3115 , y : 945}],
+			["Sol_pierre1_D", {x: 2905 , y : 735}],
+			["Sol_pierre1_D", {x: 2835 , y : 665}],
+			
+			            ];
               stage.loadAssets(levelAssets);  
-
+			  
+            //load level assets
+            
             stage.add("viewport").follow(player,{x: true, y: true},{minX: 0, maxX: background.p.w, minY: 0, maxY: background.p.h});
+		   
 		   
         });
