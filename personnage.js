777214a148d4e12c9185e76c8ba2a603db42a92d
Qu1lby <kilian.cuny@hotmail.fr>
2014-02-21 12:00:28 +0100

    Personnage

--- personnage.js
+++ personnage.js
@@ -1,9 +1,9 @@
-	//init : à la création
-	//step : à chaque instant
-	//damage : à chaque dommage
+//init : à la création
+//step : à chaque instant
+//damage : à chaque dommage
 
 	Q.Sprite.extend("Orange",{
-        init: function(p) {
+        init: function(p){
     	    this._super(p, {asset: "orange.png", x: 200, y: 100, jumpSpeed: -400, lives: 2});
     	    this.add("2d, platformerControls"); 
     	    this.p.timeInvincible = 0;
@@ -18,11 +18,10 @@
         },
 
 		// Gère le temps que le personnage est en vie -> fin du lvl
-  		step: function(dt) {	
+  		step: function(dt){	
 			if(this.p.timeInvincible == 0) {
        			var encoretjrs = new Date();				
-				if (encoretjrs.getSeconds()>this.p.date_prec.getSeconds())
-				{					
+				if (encoretjrs.getSeconds()>this.p.date_prec.getSeconds()){					
 					this.p.secondeabs++;
 					this.p.once = false;
 				}else{
@@ -50,15 +49,15 @@
 			var livesLabel = Q("UI.Text",1).first();
 			livesLabel.p.label = "Lives x "+this.p.lives;
 
-        	if(Q.inputs["left"]) {
+        	if(Q.inputs["left"]){
             	this.p.flip = "x";
             }
 
-            if(Q.inputs["right"]&& this.p.direction == "left") {
+            if(Q.inputs["right"]&& this.p.direction == "left"){
 				this.p.flip = false;                    
             }
 
-            if(this.p.timeInvincible > 0) {
+            if(this.p.timeInvincible > 0){
                	this.p.timeInvincible = Math.max(this.p.timeInvincible - dt, 0);
             }
 
@@ -75,7 +74,6 @@
 				Q.clearStages();
 				Q.stageScene("endGame",1)
 			}
-
 		
 		// Détruit le personnage s'il tombe troisième niveau
 			if(this.p.y> 1800){
@@ -92,8 +90,8 @@
 
         },
 
-        damage: function() {
-            if(!this.p.timeInvincible) {
+        damage: function(){
+            if(!this.p.timeInvincible){
                 this.p.lives--;
                 this.p.timeInvincible = 1;
                     
@@ -110,8 +108,9 @@
         },
     });
 
-		Q.Sprite.extend("Fraise",{
-        init: function(p) {
+// ------------------------------------------------------------------------------------------------------------------
+	Q.Sprite.extend("Fraise",{
+        init: function(p){
        		this._super(p, { asset: "orange.png", x: 17*70+35, y: 11*70+35, speed : 600,jumpSpeed: -800, lives: 1 });
         	this.add("2d, platformerControls"); 
         	this.p.timeInvincible = 0;
@@ -126,9 +125,179 @@
         },
 
 		// Gère le temps que le personnage est en vie -> fin du lvl
- 		step: function(dt) {	
-			if(this.p.timeInvincible == 0) {	
+ 		step: function(dt){	
+			if(this.p.timeInvincible == 0){	
        			var encoretjrs = new Date();				
+				if (encoretjrs.getSeconds()>this.p.date_prec.getSeconds()){					
+					this.p.secondeabs++;
+					this.p.once = false;
+				}else{
+					 if (this.p.seconde == 60){
+						this.p.secondeabs = 0;
+						this.p.minute ++; 
+					}
+				}
+
+				this.p.date_prec = encoretjrs;
+				this.p.seconde = this.p.secondeabs;
+				if((this.p.secondeabs<10) && (this.p.once == false)){
+					 this.p.secondeabs = "0"+this.p.secondeabs;
+					 this.p.once = true;
+				}
+				if(this.p.minute<10){ 
+					var livesLabel = Q("UI.Text",1).at(1);
+        		    livesLabel.p.label = "Temps : 0"+this.p.minute+":"+this.p.secondeabs;	
+				}else{
+					var livesLabel = Q("UI.Text",1).at(1);
+        		    livesLabel.p.label = "Temps : "+this.p.minute+":"+this.p.secondeabs;	
+				}
+			}
+
+			var livesLabel = Q("UI.Text",1).first();
+            livesLabel.p.label = "Lives x "+this.p.lives;
+
+        	if(Q.inputs["left"]){
+            	this.p.flip = "x";
+            }
+
+            if(Q.inputs["right"]&& this.p.direction == "left"){
+				this.p.flip = false;                    
+            }
+
+            if(this.p.timeInvincible > 0) {
+               	this.p.timeInvincible = Math.max(this.p.timeInvincible - dt, 0);
+            }
+
+			if(this.p.y>30*70){
+				this.destroy();
+				Q.clearStages();
+				Q.stageScene("endGame",1, {label: "Game Over"})
+			}
+        },
+
+        damage: function(){
+            if(!this.p.timeInvincible){
+                this.p.lives--;
+                this.p.timeInvincible = 1;
+                    
+                if(this.p.lives==0){
+                    this.destroy();
+					Q.clearStages();
+                    Q.stageScene("endGame",1, { label: "Game Over" }); 
+                }else{
+                    var livesLabel = Q("UI.Text",1).first();
+                    livesLabel.p.label = "Lives x "+this.p.lives;
+                }
+            }
+        },
+    });
+
+
+// ------------------------------------------------------------------------------------------------------------------
+	Q.Sprite.extend("Banane",{
+        init: function(p){
+       		this._super(p, { asset: "banane.png", x: 1505, y: 1085, jumpSpeed: -530, lives: 2});
+        	this.add("2d, platformerControls"); 
+        	this.p.timeInvincible = 0;
+			this.p.sol = 0; 	 // Retiens le dernier cube
+			this.p.first = 1; 	 // Premier cube touché -> 0 (évite bug)
+			this.p.maintenant = new Date();
+			this.p.minute = 0;
+			this.p.once = false;
+			this.p.date_prec = this.p.maintenant;
+   			this.p.secondeabs = 0; 
+        },
+
+		// Gère le temps que le personnage est en vie -> fin du lvl
+   		step: function(dt){	
+			if(this.p.timeInvincible == 0) {
+				var encoretjrs = new Date();				
+				if (encoretjrs.getSeconds()>this.p.date_prec.getSeconds()){					
+					this.p.secondeabs++;
+					this.p.once = false;
+				}else{
+					 if (this.p.seconde == 60){
+						this.p.secondeabs = 0;
+						this.p.minute ++; 
+					}
+				}
+
+				this.p.date_prec = encoretjrs;
+				this.p.seconde = this.p.secondeabs;
+				if((this.p.secondeabs<10) && (this.p.once == false)){
+					 this.p.secondeabs = "0"+this.p.secondeabs;
+					 this.p.once = true;
+				}
+				if(this.p.minute<10){ 
+					var livesLabel = Q("UI.Text",1).at(1);
+        		    livesLabel.p.label = "Temps : 0"+this.p.minute+":"+this.p.secondeabs;	
+				}else{
+					var livesLabel = Q("UI.Text",1).at(1);
+        		    livesLabel.p.label = "Temps : "+this.p.minute+":"+this.p.secondeabs;	
+				}
+			}
+
+			var livesLabel = Q("UI.Text",1).first();
+            livesLabel.p.label = "Lives x "+this.p.lives;
+
+        	if(Q.inputs["left"]){
+            	this.p.flip = "x";
+            }
+
+            if(Q.inputs["right"]&& this.p.direction == "left"){
+				this.p.flip = false;                    
+            }
+
+            if(this.p.timeInvincible > 0) {
+               	this.p.timeInvincible = Math.max(this.p.timeInvincible - dt, 0);
+            }
+
+			if(this.p.y>1645){
+				this.destroy();
+				Q.clearStages();
+				Q.stageScene("endGame",1, {label: "Game Over"})
+			}
+
+        },
+
+        damage: function(){
+            if(!this.p.timeInvincible){
+                this.p.lives--;
+                this.p.timeInvincible = 1;
+                    
+                if(this.p.lives==0) {
+                    this.destroy();
+					Q.clearStages();
+                    Q.stageScene("endGame",1, { label: "Game Over" }); 
+                }
+                else {
+                    var livesLabel = Q("UI.Text",1).first();
+                    livesLabel.p.label = "Lives x "+this.p.lives;
+                }
+            }
+        },
+    });
+
+// ------------------------------------------------------------------------------------------------------------------
+	Q.Sprite.extend("Ananas",{
+        init: function(p){
+        	this._super(p, { asset: "ananas.png", x: 105, y: 1505, jumpSpeed: -400, lives: 2});
+        	this.add("2d, platformerControls"); 
+        	this.p.timeInvincible = 0;
+			this.p.sol = 0; 	 // Retiens le dernier cube
+			this.p.first = 1; 	 // Premier cube touché -> 0 (évite bug)
+
+			this.p.maintenant = new Date();
+			this.p.minute = 0;
+			this.p.once = false;
+			this.p.date_prec = this.p.maintenant;
+   			this.p.secondeabs = 0; 
+        },
+
+		// Gère le temps que le personnage est en vie -> fin du lvl
+  		step: function(dt){	
+			if(this.p.timeInvincible == 0){
+				var encoretjrs = new Date();				
 				if (encoretjrs.getSeconds()>this.p.date_prec.getSeconds())
 				{					
 					this.p.secondeabs++;
@@ -158,97 +327,11 @@
 			var livesLabel = Q("UI.Text",1).first();
             livesLabel.p.label = "Lives x "+this.p.lives;
 
-        	if(Q.inputs["left"]) {
+        	if(Q.inputs["left"]){
             	this.p.flip = "x";
             }
 
-            if(Q.inputs["right"]&& this.p.direction == "left") {
-				this.p.flip = false;                    
-            }
-
-            if(this.p.timeInvincible > 0) {
-               	this.p.timeInvincible = Math.max(this.p.timeInvincible - dt, 0);
-            }
-			
-			
-			if(this.p.y>30*70){
-				this.destroy();
-				Q.clearStages();
-				Q.stageScene("endGame",1, {label: "Game Over"})
-			}
-        },
-
-        damage: function() {
-            if(!this.p.timeInvincible) {
-                this.p.lives--;
-                this.p.timeInvincible = 1;
-                    
-                if(this.p.lives==0) {
-                    this.destroy();
-					Q.clearStages();
-                    Q.stageScene("endGame",1, { label: "Game Over" }); 
-                }
-                else {
-                    var livesLabel = Q("UI.Text",1).first();
-                    livesLabel.p.label = "Lives x "+this.p.lives;
-                }
-            }
-        },
-    });
-
-
-	Q.Sprite.extend("Banane",{
-        init: function(p) {
-       		this._super(p, { asset: "banane.png", x: 1505, y: 1085, jumpSpeed: -530, lives: 2});
-        	this.add("2d, platformerControls"); 
-        	this.p.timeInvincible = 0;
-			this.p.sol = 0; 	 // Retiens le dernier cube
-			this.p.first = 1; 	 // Premier cube touché -> 0 (évite bug)
-			this.p.maintenant = new Date();
-			this.p.minute = 0;
-			this.p.once = false;
-			this.p.date_prec = this.p.maintenant;
-   			this.p.secondeabs = 0; 
-        },
-
-		// Gère le temps que le personnage est en vie -> fin du lvl
-   		step: function(dt) {	
-			if(this.p.timeInvincible == 0) {
-				var encoretjrs = new Date();				
-				if (encoretjrs.getSeconds()>this.p.date_prec.getSeconds())
-				{					
-					this.p.secondeabs++;
-					this.p.once = false;
-				}else{
-					 if (this.p.seconde == 60){
-						this.p.secondeabs = 0;
-						this.p.minute ++; 
-						}
-				}
-
-				this.p.date_prec = encoretjrs;
-				this.p.seconde = this.p.secondeabs;
-				if((this.p.secondeabs<10) && (this.p.once == false)){
-					 this.p.secondeabs = "0"+this.p.secondeabs;
-					 this.p.once = true;
-				}
-				if(this.p.minute<10){ 
-					var livesLabel = Q("UI.Text",1).at(1);
-        		    livesLabel.p.label = "Temps : 0"+this.p.minute+":"+this.p.secondeabs;	
-				}else{
-					var livesLabel = Q("UI.Text",1).at(1);
-        		    livesLabel.p.label = "Temps : "+this.p.minute+":"+this.p.secondeabs;	
-				}
-			}
-
-			var livesLabel = Q("UI.Text",1).first();
-            livesLabel.p.label = "Lives x "+this.p.lives;
-
-        	if(Q.inputs["left"]) {
-            	this.p.flip = "x";
-            }
-
-            if(Q.inputs["right"]&& this.p.direction == "left") {
+            if(Q.inputs["right"]&& this.p.direction == "left"){
 				this.p.flip = false;                    
             }
 
@@ -264,104 +347,16 @@
 
         },
 
-        damage: function() {
-            if(!this.p.timeInvincible) {
+        damage: function(){
+            if(!this.p.timeInvincible){
                 this.p.lives--;
                 this.p.timeInvincible = 1;
                     
-                if(this.p.lives==0) {
+                if(this.p.lives==0){
                     this.destroy();
 					Q.clearStages();
                     Q.stageScene("endGame",1, { label: "Game Over" }); 
-                }
-                else {
-                    var livesLabel = Q("UI.Text",1).first();
-                    livesLabel.p.label = "Lives x "+this.p.lives;
-                }
-            }
-        },
-    });
-
-
-	Q.Sprite.extend("Ananas",{
-        init: function(p) {
-        	this._super(p, { asset: "ananas.png", x: 105, y: 1505, jumpSpeed: -400, lives: 2});
-        	this.add("2d, platformerControls"); 
-        	this.p.timeInvincible = 0;
-			this.p.sol = 0; 	 // Retiens le dernier cube
-			this.p.first = 1; 	 // Premier cube touché -> 0 (évite bug)
-
-			this.p.maintenant = new Date();
-			this.p.minute = 0;
-			this.p.once = false;
-			this.p.date_prec = this.p.maintenant;
-   			this.p.secondeabs = 0; 
-        },
-
-		// Gère le temps que le personnage est en vie -> fin du lvl
-  		step: function(dt) {	
-			if(this.p.timeInvincible == 0) {
-				var encoretjrs = new Date();				
-				if (encoretjrs.getSeconds()>this.p.date_prec.getSeconds())
-				{					
-					this.p.secondeabs++;
-					this.p.once = false;
-				}else{
-					 if (this.p.seconde == 60){
-						this.p.secondeabs = 0;
-						this.p.minute ++; 
-						}
-				}
-
-				this.p.date_prec = encoretjrs;
-				this.p.seconde = this.p.secondeabs;
-				if((this.p.secondeabs<10) && (this.p.once == false)){
-					 this.p.secondeabs = "0"+this.p.secondeabs;
-					 this.p.once = true;
-				}
-				if(this.p.minute<10){ 
-					var livesLabel = Q("UI.Text",1).at(1);
-        		    livesLabel.p.label = "Temps : 0"+this.p.minute+":"+this.p.secondeabs;	
-				}else{
-					var livesLabel = Q("UI.Text",1).at(1);
-        		    livesLabel.p.label = "Temps : "+this.p.minute+":"+this.p.secondeabs;	
-				}
-			}
-
-			var livesLabel = Q("UI.Text",1).first();
-            livesLabel.p.label = "Lives x "+this.p.lives;
-
-        	if(Q.inputs["left"]) {
-            	this.p.flip = "x";
-            }
-
-            if(Q.inputs["right"]&& this.p.direction == "left") {
-				this.p.flip = false;                    
-            }
-
-            if(this.p.timeInvincible > 0) {
-               	this.p.timeInvincible = Math.max(this.p.timeInvincible - dt, 0);
-            }
-
-			if(this.p.y>1645){
-				this.destroy();
-				Q.clearStages();
-				Q.stageScene("endGame",1, {label: "Game Over"})
-			}
-
-        },
-
-        damage: function() {
-            if(!this.p.timeInvincible) {
-                this.p.lives--;
-                this.p.timeInvincible = 1;
-                    
-                if(this.p.lives==0) {
-                    this.destroy();
-					Q.clearStages();
-                    Q.stageScene("endGame",1, { label: "Game Over" }); 
-                }
-                else {
+                } else{
                     var livesLabel = Q("UI.Text",1).first();
                     livesLabel.p.label = "Lives x "+this.p.lives;
                 }
