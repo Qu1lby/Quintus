6924fe47e8f87f327adc2f959e14b130e51a2578
Leclouerec <leclouerec@gmx.fr>
2014-02-26 22:25:12 +0100

    ze'g

--- Components.js
+++ Components.js
@@ -458,3 +458,36 @@
 			this.add("2d2, cube");
         }
     });	
+	
+	// balle tiré
+/*  Q.Sprite.extend('CannonBall',{
+    init: function(props) {
+      this._super({
+        shape: 'circle',
+        color: 'red',
+        r: 8,
+        restitution: 0.5,
+        density: 4,
+        x: props.dx * 50 + 10,
+        y: props.dy * 50 + 210,
+        seconds: 5
+      });
+      this.add('physics');
+      this.on('step',this,'countdown');
+    },
+ 
+    countdown: function(dt) {
+      this.p.seconds -= dt;
+      if(this.p.seconds < 0) { 
+        this.destroy();
+      } else if(this.p.seconds < 1) {
+        this.set({ "fill-opacity": this.p.seconds });
+      }
+    },
+	
+	 collision: function(col) {
+ 		   collision.obj.damage();
+          this.destroy();
+		  },
+  });*/
+ 
--- ennemis.js
+++ ennemis.js
@@ -50,6 +50,50 @@
 		}
 	});
 	
+	//Comportement des ennemmies qui tirent
+	Q.Sprite.extend("DrawEnemy", {
+    	init: function(p) {
+        	this._super(p, {vx: 0, defaultDirection: "left"});
+            this.add("2d, aiBounce, SpecialEnemy");
+        },
+        
+		step: function(dt) {        
+        	var dirX = this.p.vx/Math.abs(this.p.vx);
+            var ground = Q.stage().locate(this.p.x, this.p.y + this.p.h/2 + 1, Q.SPRITE_DEFAULT);
+            var nextTile = Q.stage().locate(
+					this.p.x + dirX * this.p.w/2 + dirX, this.p.y + this.p.h/2 + 1, Q.SPRITE_DEFAULT);  		
+				
+            if(!nextTile && ground) {
+            	if(this.p.vx > 0) {
+                	if(this.p.defaultDirection == "right") {
+                	    this.p.flip = "x";
+						Q.stage(0).cannon.fire();
+                    }else{
+                        this.p.flip = false;
+						//Q.stage(0).cannon.fire();
+                    }
+                } else {
+                    if(this.p.defaultDirection == "left") {
+                        this.p.flip = "x";
+						//Q.stage(0).cannon.fire();
+                    }else{
+                        this.p.flip = false;
+						//Q.stage(0).cannon.fire();
+                    }
+                }
+                this.p.vx = -this.p.vx;
+            }
+        },
+		
+		fire: function() {
+			var dx = 45,
+			dy = 45,
+			ball = new Q.CannonBall({ dx: dx, dy: dy, angle: this.p.angle });
+			Q.stage().insert(ball);
+			ball.physics.velocity(dx*400,dy*400);
+    },
+        });
+	
 //Comportement for common enemy behaviors
 	Q.component("commonEnemy", {
     	added: function() {
@@ -70,6 +114,30 @@
 				   (collision.obj.isA("Banane")) || (collision.obj.isA("Ananas"))) {  
                 	collision.obj.p.vy = -100;
                     this.destroy();
+                }
+            });
+        },        
+    });
+	
+	//ennemie special
+		Q.component("SpecialEnemy", {
+    	added: function() {
+        	var entity = this.entity;
+            entity.on("bump.left,bump.right,bump.bottom",function(collision) {
+           		if((collision.obj.isA("Orange")) || (collision.obj.isA("Fraise")) || 
+				   (collision.obj.isA("Banane")) || (collision.obj.isA("Ananas"))) {                   
+					
+					if(music){
+						Q.stage(0).DrawEnemy.fire();
+					}
+                }
+        });
+				
+        entity.on("bump.top",function(collision) {
+              	if((collision.obj.isA("Orange")) || (collision.obj.isA("Fraise")) ||
+				   (collision.obj.isA("Banane")) || (collision.obj.isA("Ananas"))) {  
+                	collision.obj.p.vy = -100;
+                    Q.stage(0).DrawEnemy.fire();
                 }
             });
         },        
