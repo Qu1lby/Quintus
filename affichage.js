8b1ddbaf6a8039e64ff0190987815ef01ca7c986
Qu1lby <kilian.cuny@hotmail.fr>
2014-02-28 12:24:32 +0100

    Qui marche

--- affichage.js
+++ affichage.js
@@ -68,7 +68,6 @@
 		men.on("click",function() {
 			Q.clearStages();
       		Q.stageScene('niveau');
-			Q.stageScene('Audio',4);
 			scene_courante = "niveau";
 			scene_prec = "menu";
      	});	
@@ -192,7 +191,6 @@
       			Q.stageScene('level1');
 				Q.stageScene("gameStats",1);
 				Q.stageScene('tut1',2);
-				Q.stageScene('Audio',4);
 				
 				if(music){
 					Q.audio.play('lvl1.mp3',{ loop: true });
@@ -206,7 +204,6 @@
 				Q.clearStages();
       			Q.stageScene('level2');
 				Q.stageScene("gameStats",1);
-				Q.stageScene('Audio',4);
 				
 				if(music){
 					Q.audio.play('lvl2.mp3',{ loop: true });
@@ -220,7 +217,6 @@
 				Q.clearStages();
       			Q.stageScene('level3');
 				Q.stageScene("gameStats",1);
-				Q.stageScene('Audio',4);
 				
 				scene_courante = "lvl3";
 				scene_prec = "niveau";
@@ -230,7 +226,6 @@
 				Q.clearStages();
       			Q.stageScene('level4');
 				Q.stageScene("gameStats",1);
-				Q.stageScene('Audio',4);
 				
 				scene_courante = "lvl4";
 				scene_prec = "niveau";
@@ -305,7 +300,6 @@
 		msg.on("click",function() {
        		Q.clearStages();
    			Q.stageScene('niveau');
-			Q.stageScene('Audio',4);
 				
 			scene_prec = scene_courante
 			scene_courante = "niveau";
@@ -371,7 +365,6 @@
 		msg.on("click",function() {
        		Q.clearStages();
    			Q.stageScene('niveau');
-			Q.stageScene('Audio',4);
 			
 			Q.audio.stop();
 			music = false;
@@ -411,8 +404,43 @@
 				family : "comic sans ms",
 				size : 21
             }),statsContainer);	
+			
+		var aud = stage.insert(new Q.UI.Button({
+			fill: "transparent",
+ 	        asset: "audio.png",
+			opacity: 0.5,
+        	x: wi-20,
+        	y: 20,
+        	w: 29,
+        	h: 28
+   		}));
+			
+		aud.on("click",function() {
+		if (scene_courante != null && !pause && !music){	
+			if (scene_courante == "lvl1"){
+				Q.audio.play('lvl1.mp3',{ loop: true });
+				music = true;
+			}
+			
+			if (scene_courante == "lvl2"){
+				Q.audio.play('lvl2.mp3',{ loop: true });
+				music = true;
+			}
+			
+			if (scene_courante == "lvl3"){
+			}
+			
+			if (scene_courante == "lvl4"){
+			}
+			
+		}else{
+			Q.audio.stop(); 
+			music = false;
+		}
 		});
-
+		
+		});
+		
 // Pause
 	Q.scene("PauseAff", function(stage) {
 		var Opacite = stage.insert(new Q.UI.Container({
@@ -480,7 +508,6 @@
 		msg.on("click",function() {
        		Q.clearStages();
    			Q.stageScene('niveau');
-			Q.stageScene('Audio',4);
 			
 			Q.audio.stop();
 			music = false;
@@ -498,41 +525,6 @@
 	
 // Reinitialiser un stage
 	Q.scene("Blanc", function(stage){
-	});
-	
-	Q.scene("Audio", function(stage) {
- 	   var aud = stage.insert(new Q.UI.Button({
-			fill: "transparent",
- 	       asset: "audio.png",
-        	x: wi-20,
-        	y: 20,
-        	w: 29,
-        	h: 28
-   		}));
-			
-		aud.on("click",function() {
-		if (scene_courante != null && !pause && !music){	
-			if (scene_courante == "lvl1"){
-				Q.audio.play('lvl1.mp3',{ loop: true });
-				music = true;
-			}
-			
-			if (scene_courante == "lvl2"){
-				Q.audio.play('lvl2.mp3',{ loop: true });
-				music = true;
-			}
-			
-			if (scene_courante == "lvl3"){
-			}
-			
-			if (scene_courante == "lvl4"){
-			}
-			
-		}else{
-			Q.audio.stop(); 
-			music = false;
-		}
-		});
 	});
 	
 /*
