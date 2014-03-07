6cc118bda3b26cfe2e93b2f672c0f397b5ef1c6e
Qu1lby <kilian.cuny@hotmail.fr>
2014-03-07 18:59:44 +0100

    Bacon

--- personnage.js
+++ personnage.js
@@ -96,7 +96,7 @@
             if(this.p.timeInvincible > 0){
                	this.p.timeInvincible = Math.max(this.p.timeInvincible - dt, 0);
             }
-/*
+
 		// Détruit le personnage s'il tombe premier niveau
 			if(this.p.y> 420 && this.p.y<470 && this.p.x<1800){
 				this.destroy();
@@ -126,7 +126,7 @@
 				scene_prec = "lvl1";
                 
 			}
-*/			
+			
 			if(this.p.x> 1850 && this.p.y>500 && !this.p.tut3){
 				Q.stageScene("tut3",2);
 			}
